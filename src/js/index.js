398f62ce428cdfc9f7dd1dc171ce05520e1a9a8d
truncations <do.elements@outlook.com>
2026-08-22 11:22:00 -0500

    Fixes to parts of the website: - Bowling ASCII text better positioned - Small bar in the header section kept being misaligned by 1 pixel - Improper padding with main content/scroll-bar off-aligned by a few pixels.
    Added some more style to the website, like customizing the scroll bar. Added some animations.

---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+var outer_text_element;
+const welcome_texts = [".self", "...welcome", "greninja!!", "gren..?", "ghostkeeper."];
+
+window.onload = function onLoad() {
+    outer_text_element = document.getElementById("outer-text");
+    outer_text_element.textContent = welcome_texts[Math.floor(Math.random() * welcome_texts.length)]
+}
